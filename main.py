06fac56cb9ca7c74f992abeca01d2b370b505bf5
stay-ghosting <rheonmais@gmail.com>
2023-04-10 10:00:11 +0100

    stable

--- main.py
+++ main.py
@@ -1,35 +1,120 @@
 import tkinter
-import customtkinter
+from tkinter import filedialog
+import customtkinter as ctk
 import subprocess
-import threading
+import ripper
+import os
+import enum
 
 # TODO create json save - recents/ key
 # TODO stop crashing
 #
 
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
+app = ctk.CTk()
 
-app = customtkinter.CTk()
-app.geometry("800x240")
-app.title("YT Ripper")
+url_input = ctk.StringVar()
 
-url_input = customtkinter.StringVar()
+dir_input = ctk.StringVar()
+dir_input.set("")
+
+error_message = ctk.StringVar()
+error_message.set("")
+error_type = None
+
+class Error(enum.Enum):
+    DirectoryError = 1
+    URLError = 2
+
+def on_progress_callback(stream, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    file_progress = bytes_downloaded / total_size * 100
+    file_progress_bar.set(file_progress)
+
+def on_complete_callback(files_downloaded, amount_of_files):
+    print(f"amount {amount_of_files}")
+    print(f"completed {files_downloaded}")
+    all_files_progress_bar.set(files_downloaded/ amount_of_files)
 
 def on_download():
+    global error_type
     url = url_input.get()
-    subprocess.run(f"python ripper.py {url}")
+    dir = dir_input.get()
 
-# Use CTkButton instead of tkinter Button
-main = customtkinter.CTkFrame(master=app)
+    # if directory does NOT exist ...
+    if not os.path.exists(dir):
+        error_message.set("Please select a valid folder")
+        error_type = Error.DirectoryError
+        return
+    
+    try:
+        files_in_folder, files_to_download = ripper.filter_playlist(url, dir)
+    except KeyError:
+        error_message.set("Please select a valid url")
+        error_type = Error.URLError
+        return
+    else:
+        ripper.download_audio(files_to_download, dir, on_progress_callback, on_complete_callback)
 
-lbl_url = customtkinter.CTkLabel(main, text="Playlist URL:")
-ety_url = customtkinter.CTkEntry(main, textvariable=url_input, placeholder_text="https://www.youtube.com/playlist?list=", width=500)
-btn_download = customtkinter.CTkButton(master=main, text="Start Download", command=lambda: on_download())
+def on_change_directory():
+    dir_input.set(filedialog.askdirectory())
+    if error_type == Error.DirectoryError:
+        error_message.set("")
 
-lbl_url.pack(pady=(20,10))
-ety_url.pack(padx=20, pady=(0,20))
-btn_download.pack(pady=(0,30))
-main.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+def on_url_change(e):
+    if error_type == Error.URLError:
+        error_message.set("")
+
+
+
+
+
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
+app.geometry("800x500")
+app.title("YT Ripper")
+
+
+main = ctk.CTkFrame(app)
+
+lbl_url = ctk.CTkLabel(main, text="Playlist URL:")
+ety_url = ctk.CTkEntry(main, textvariable=url_input, placeholder_text="https://www.youtube.com/playlist?list=", width=500, border_width=0)
+ety_url.bind("<Key>", on_url_change)
+
+frm_dir = ctk.CTkFrame(main)
+btn_open_dir = ctk.CTkButton(frm_dir, text="Open Folder", command=on_change_directory)
+lbl_dir_title = ctk.CTkLabel(frm_dir, text=f"Output folder:")
+lbl_dir = ctk.CTkLabel(frm_dir, textvariable=dir_input)
+
+lbl_error_message = ctk.CTkLabel(main, textvariable=error_message, text_color="red")
+
+lbl_file_progress_bar = ctk.CTkLabel(main, text=f"File Progress:")
+file_progress_bar = ctk.CTkProgressBar(main)
+file_progress_bar.set(0)
+lbl_all_files_progress_bar = ctk.CTkLabel(main, text=f"Playlist Progress:")
+all_files_progress_bar = ctk.CTkProgressBar(main)
+all_files_progress_bar.set(0)
+
+btn_download = ctk.CTkButton(main, text="Start Download", command=lambda: on_download())
+
+frm_dir.pack(fill=ctk.BOTH, padx=20, pady=(30, 0))
+lbl_dir_title.grid(column=0, row=0, sticky=ctk.W, padx=(10, 0), pady=(10, 0))
+lbl_dir.grid(column=0, row=1, sticky=ctk.W, padx=(10, 0))
+btn_open_dir.grid(column=1, row=0, rowspan=2, sticky=ctk.NE, padx=(0, 10), pady=(10, 0))
+frm_dir.columnconfigure(0, weight=1)
+frm_dir.columnconfigure(1, weight=1)
+
+lbl_url.pack(pady=(40,5), anchor=ctk.W, padx=(30,0))
+ety_url.pack(padx=20, pady=(0,0))
+
+lbl_error_message.pack(pady=5)
+lbl_file_progress_bar.pack(anchor=ctk.W, padx=(30,0))
+file_progress_bar.pack(fill=ctk.BOTH, padx=20, pady=(5, 15))
+lbl_all_files_progress_bar.pack(anchor=ctk.W, padx=(30,0))
+all_files_progress_bar.pack(fill=ctk.BOTH, padx=20, pady=5)
+
+btn_download.pack(pady=(30,30))
+main.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
 
 app.mainloop()
--- ripper.py
+++ ripper.py
@@ -1,6 +1,6 @@
 import os
 import pytube
-from typing import Tuple
+from typing import Tuple, Callable
 
 # https://www.youtube.com/playlist?list=PLo80Q9Yj8XHfHtCXfrp81qRw5-PtLP-TG
 def file_exists_in_folder(url: str, dir: str):
@@ -9,6 +9,8 @@ def file_exists_in_folder(url: str, dir: str):
     # get the title
     video_id = url.split("=")[1]
     video_id_formatted = f"[{video_id}]"
+    # video = pytube.YouTube(url) TODO
+    # video.video_id
     # get names of every file in directory
     filesNames = [f for f in os.listdir(dir) 
                   if os.path.isfile(os.path.join(dir, f))]
@@ -24,11 +26,19 @@ def file_exists_in_folder(url: str, dir: str):
         # return false
         return False
   
-def download_audio(videos: list[pytube.YouTube], dir: str):
+def download_audio(videos: list[pytube.YouTube], dir: str, 
+                   on_progress_callback: Callable[[any, int], None],
+                   on_complete_callback: Callable[[int, int], None]):
     """Gets list of YouTube objects and coverts them to mp3.
     Also adds video id at the end of the title"""
     # for each video ...
-    for video in videos:
+    for i, video in enumerate(videos):
+        files_downloaded = i + 1
+        amount_of_files = len(videos)
+        video.register_on_progress_callback(lambda stream, chunk, bytes_remaining: 
+                                            on_progress_callback(stream, bytes_remaining))
+        video.register_on_complete_callback(lambda stream, file_path:
+                                            on_complete_callback(files_downloaded, amount_of_files))
         try:
             # try create an audio stream
             audio = video.streams.filter(only_audio=True).all()[0]
@@ -41,7 +51,7 @@ def download_audio(videos: list[pytube.YouTube], dir: str):
             # add it to the title and set it to mp3 file
             new_name = video.title.replace('"', '') + f"[{video_id}].mp3"
             # download the file
-            audio.download(output_path=dir, filename=new_name)
+            audio.download(dir, filename=new_name)
 
 def filter_playlist(playlist_url: str, dir:str) -> Tuple[pytube.YouTube, pytube.YouTube]:
     """Filters playlist into:
@@ -56,7 +66,7 @@ def filter_playlist(playlist_url: str, dir:str) -> Tuple[pytube.YouTube, pytube.
     # for video in playlist ...
     for video in playlist.videos:
         # if in directory already ...
-        if file_exists_in_folder(video.watch_url, dir):
+        if file_exists_in_folder(video.watch_url, dir) or video.age_restricted == True:
             # add to files in folder list
             files_in_folder.append(video)
         else:
@@ -64,3 +74,10 @@ def filter_playlist(playlist_url: str, dir:str) -> Tuple[pytube.YouTube, pytube.
             files_to_download.append(video)
     # return files list in tuple
     return files_in_folder, files_to_download
+
+
+# url = 'https://www.youtube.com/playlist?list=PLo80Q9Yj8XHfHtCXfrp81qRw5-PtLP-TG'
+# dir = "D:/ribby/Documents/Work/python/ripper/songs"
+
+# files_in_folder, files_to_download = filter_playlist(url, dir)
+# download_audio(files_to_download, dir)
