5aaa5337a4769b50fa61ce572fa87b572a80e4a9
stay-ghosting <rheonmais@gmail.com>
2023-04-11 03:37:05 +0100

    loading bars freeze fix

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from tkinter import filedialog
 import customtkinter as ctk
-import ripper
+from ripper import Ripper
 import os
-import enum
-import concurrent.futures
 import threading
-import asyncio
-import time
 from threadWithReturn import ThreadWithReturnValue
+from error import *
 
 
 # TODO create json save - recents/ key
@@ -17,34 +14,46 @@ from threadWithReturn import ThreadWithReturnValue
 app = ctk.CTk()
 
 url_input = ctk.StringVar()
-url_input.set("https://www.youtube.com/playlist?list=PLo80Q9Yj8XHfHtCXfrp81qRw5-PtLP-TG")
 
 dir_input = ctk.StringVar()
-dir_input.set("D:/ribby/Documents/Work/python/ripper/songs")
 
 error_message = ctk.StringVar()
+
+lbl_file_load_progress_bar_text = ctk.StringVar()
+def set_loading_files_data(files_loaded, amount_of_files):
+    file_load_progress_bar.set(files_loaded / amount_of_files)
+    return f"File Load Progress: {files_loaded} / {amount_of_files}"
+
+lbl_all_files_progress_bar_text = ctk.StringVar()
+def set_downloading_files_data(files_downloaded, amount_of_files):
+    all_files_progress_bar.set(files_downloaded/ amount_of_files)
+    return f"Download Progress: {files_downloaded} / {amount_of_files}"
+
+# set initial values
+
+url_input.set("https://www.youtube.com/playlist?list=PLo80Q9Yj8XHfHtCXfrp81qRw5-PtLP-TG")
+dir_input.set("D:/ribby/Documents/Work/python/ripper/songs")
+
 error_message.set("")
-error_type = None
+error_type : Error = None
 
-class Error(enum.Enum):
-    DirectoryError = 1
-    URLError = 2
+# lbl_file_load_progress_bar_text.set(set_loading_files_data(0, 0))
+# lbl_all_files_progress_bar_text.set(set_downloading_files_data(0, 0))
 
-def on_progress_callback(stream, bytes_remaining):
-    total_size = stream.filesize
-    bytes_downloaded = total_size - bytes_remaining
-    file_progress = bytes_downloaded / total_size * 100
-    file_progress_bar.set(file_progress)
+
+def on_file_loaded(files_loaded, amount_of_files):
+    set_loading_files_data(files_loaded, amount_of_files)
 
 def on_complete_callback(files_downloaded, amount_of_files):
-    print(f"amount {amount_of_files}")
-    print(f"completed {files_downloaded}")
-    all_files_progress_bar.set(files_downloaded/ amount_of_files)
+    set_downloading_files_data(files_downloaded, amount_of_files)
+
 
 def on_download():
     global error_type
-    url = url_input.get()
+    playlist_url = url_input.get()
     dir = dir_input.get()
+
+    ripper = Ripper(dir, playlist_url)
 
     # if directory does NOT exist ...
     if not os.path.exists(dir):
@@ -53,13 +62,13 @@ def on_download():
         return
     
     try:
-        files_in_folder, files_to_download = ripper.filter_playlist(url, dir)
+        threading.Thread(target=lambda:ripper.filter_playlist(on_file_loaded)).start()
     except KeyError:
         error_message.set("Please select a valid url")
         error_type = Error.URLError
         return
     else:
-        threading.Thread(target=lambda:ripper.download_audio(files_to_download, dir, on_progress_callback, on_complete_callback)).start()
+        threading.Thread(target=lambda:ripper.download_audio(on_complete_callback)).start()
 
 def on_change_directory():
     dir_input_temp = filedialog.askdirectory()
@@ -98,10 +107,10 @@ lbl_dir = ctk.CTkLabel(frm_dir, textvariable=dir_input)
 
 lbl_error_message = ctk.CTkLabel(main, textvariable=error_message, text_color="red")
 
-lbl_file_progress_bar = ctk.CTkLabel(main, text=f"File Progress:")
-file_progress_bar = ctk.CTkProgressBar(main)
-file_progress_bar.set(0)
-lbl_all_files_progress_bar = ctk.CTkLabel(main, text=f"Playlist Progress:")
+lbl_file_load_progress_bar = ctk.CTkLabel(main, textvariable=lbl_file_load_progress_bar_text)
+file_load_progress_bar = ctk.CTkProgressBar(main)
+file_load_progress_bar.set(0)
+lbl_all_files_progress_bar = ctk.CTkLabel(main, textvariable=lbl_all_files_progress_bar_text)
 all_files_progress_bar = ctk.CTkProgressBar(main)
 all_files_progress_bar.set(0)
 
@@ -118,8 +127,8 @@ lbl_url.pack(pady=(40,5), anchor=ctk.W, padx=(30,0))
 ety_url.pack(padx=20, pady=(0,0))
 
 lbl_error_message.pack(pady=5)
-lbl_file_progress_bar.pack(anchor=ctk.W, padx=(30,0))
-file_progress_bar.pack(fill=ctk.BOTH, padx=20, pady=(5, 15))
+lbl_file_load_progress_bar.pack(anchor=ctk.W, padx=(30,0))
+file_load_progress_bar.pack(fill=ctk.BOTH, padx=20, pady=(5, 15))
 lbl_all_files_progress_bar.pack(anchor=ctk.W, padx=(30,0))
 all_files_progress_bar.pack(fill=ctk.BOTH, padx=20, pady=5)
 
