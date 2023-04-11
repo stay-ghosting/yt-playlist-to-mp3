a1a2e79e64a59d929f90815701fce3698204bd53
stay-ghosting <rheonmais@gmail.com>
2023-04-11 02:21:30 +0100

    before fix file load bar

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-import concurrent.futures
-import time
+import re
 
-def foo():
-    time.sleep(1)
-    return 2
-
-with concurrent.futures.ProcessPoolExecutor() as ex:
-    t = ex.submit(foo)
-    # v = ex.submit(foo)
-    # print(t.result())
-    # print(v.result())
+print(re.sub(r"[^\w\s]", r"", "£$%^&abc"))
