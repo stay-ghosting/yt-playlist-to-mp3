995551abae64ccf6888a77b347770db04eb7ea0d
stay-ghosting <rheonmais@gmail.com>
2023-04-11 06:58:26 +0100

    covert main to a class

--- threadWithError.py
+++ threadWithError.py
@@ -1,14 +1,21 @@
 from threading import Thread
 
-class ThreadWithReturnValue(Thread): 
+class ThreadWithError(Thread): 
     def __init__(self, group=None, target=None, name=None,
                  args=(), kwargs={}, Verbose=None):
         Thread.__init__(self, group, target, name, args, kwargs)
+        self._error = None
         self._return = None
 
     def run(self):
         if self._target is not None:
-            self._return = self._target(*self._args, **self._kwargs)
+            try:
+                self._return = self._target(*self._args, **self._kwargs)
+            except Exception as e:
+                self._error = e
+
     def join(self, *args):
         Thread.join(self, *args)
+        if self._error != None:
+            raise self._error
         return self._return
