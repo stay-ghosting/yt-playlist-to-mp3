41770ae4ca5367164d4508da7978a9064567b42c
stay-ghosting <rheonmais@gmail.com>
2023-04-12 01:18:14 +0100

    added temp folder

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-from threadQueue import ThreadQueue
-from threading import Thread
-import time
-from queue import Queue, Empty
-
-
-
-
-
-# t2 = Thread(target=z)
-# t2.start()
-
+print("xxyy".replace("y", ""))
